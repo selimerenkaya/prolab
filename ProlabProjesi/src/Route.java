c730a7900a66045a7070c1ec85ce527c6a648c02
Selim Eren Kaya <121975241+selimerenkaya@users.noreply.github.com>
2023-12-03 20:27:07 +0300

    Güzergah classı düzenleme (bitmedi)
    Hoca biraz saçma saçma değerler verdiği için proje pdfinde bitiremiyorum. Şehirler arası kilometre hesabı bozuk. Bir şekilde çözüm bulup halletmeyi planlıyorum.

--- ProlabProjesi/src/Route.java
+++ ProlabProjesi/src/Route.java
@@ -1,2 +1,29 @@
+import java.util.ArrayList;
+
 public class Route {
+    String[] guzergah;
+    String ulasim_turu; // Demiryolu, Karayolu, Havayolu gibi
+
+    // Güzergah değişkeni için Get/Set metotları
+    public String[] get_guzergah(){
+        return guzergah;
+    }
+
+    public void set_guzergah (String[] guzergahGirdisi) {
+        guzergah = guzergahGirdisi;
+    }
+
+
+
+    // Parametresiz Construct Metodu
+    Route () {
+
+    }
+
+    // Güzergah oluşturulurken kullanılan Construct Metodu
+    Route (String[] guzergahGirdisi, String ulasim_turuGirdisi) {
+        guzergah = guzergahGirdisi;
+        ulasim_turu = ulasim_turuGirdisi;
+    }
+
 }
--- ProlabProjesi/src/Transport.java
+++ ProlabProjesi/src/Transport.java
@@ -9,6 +9,9 @@ public class Transport implements Ireservable {
     // Şirketleri (Firmalar) Tutan ArrayList
     static ArrayList<Company> sirketler = new ArrayList<>();
 
+    // Güzergahları Tutan ArrayList
+    static ArrayList<Route> guzergahListesi = new ArrayList<>();
+
     // Ödevde daha önceden verilmiş şirketlerin bilgileriyle beraber oluşturulması
     public void HazirBilgileriOlustur() {
         // Daha önceden verilen şirketlerin oluşturulması
@@ -21,6 +24,32 @@ public class Transport implements Ireservable {
         // Şirket Listesine eklenmeleri
         Transport hazirBilgiler = new Transport();
         hazirBilgiler.FirmaListesineEkle(A, B, C, D, F);
+
+        // Demiryolu Güzergahları
+        String[] guzer_1 = {"İstanbul", "Kocaeli", "Bilecik", "Eskişehir", "Ankara", "Eskişehir",
+                "Bilecik", "Kocaeli", "İstanbul"};
+        String[] guzer_2 = {"İstanbul", "Kocaeli", "Bilecik", "Eskişehir", "Konya", "Eskişehir",
+                "Bilecik", "Kocaeli", "İstanbul"};
+
+        // Karayolu Güzergahları
+        String[] guzer_3 = {"İstanbul", "Kocaeli", "Ankara", "Kocaeli", "İstanbul"};
+        String[] guzer_4 = {"İstanbul", "Kocaeli",  "Eskişehir", "Konya", "Eskişehir", "Kocaeli", "İstanbul"};
+
+        // Havayolu Güzergahları
+        String[] guzer_5 = {"İstanbul", "Konya", "İstanbul"};
+        String[] guzer_6 = {"İstanbul", "Ankara", "İstanbul"};
+
+        // Güzergahların oluşturulması
+        Route guzergah_1 = new Route(guzer_1, "Demiryolu");
+        Route guzergah_2 = new Route(guzer_2, "Demiryolu");
+        Route guzergah_3 = new Route(guzer_3, "Karayolu");
+        Route guzergah_4 = new Route(guzer_4, "Karayolu");
+        Route guzergah_5 = new Route(guzer_5, "Havayolu");
+        Route guzergah_6 = new Route(guzer_6, "Havayolu");
+
+        // Güzergahların güzergah listesine eklenmesi
+        hazirBilgiler.GuzergahListesineEkle(guzergah_1, guzergah_2, guzergah_3, guzergah_4, guzergah_5, guzergah_6);
+
 
     }
 
@@ -41,7 +70,24 @@ public class Transport implements Ireservable {
         return sirketler;
     }
 
-    // Güzergah Bilgileri
+
+    // Güzergah listesine güzergah eklemeyi sağlayan metot
+    public void GuzergahListesineEkle(Route... guzergahlar) {
+        // Birden fazla firma aynı anda eklenebilir halde
+        guzergahListesi.addAll(Arrays.asList(guzergahlar));
+    }
+
+    // Güzergah listesinden güzergah silmeye yarayan metot
+    public void GuzergahListesindenSil(Route guzergah) {
+        // Tek seferde sadece tek güzergah silinebilir
+        guzergahListesi.remove(guzergah);
+    }
+
+    // Güzergah bilgilerini döndüren metot
+    public ArrayList<Route> GuzergahBilgileriniDondur() {
+        return guzergahListesi;
+    }
+
 
 
     // Koltuk Durum Bilgisi Fonksiyonu
